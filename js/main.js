51d89040a8da05fc4e3a33c02fa03fe07eb62ef3
Cristian Girão <cristiangirao97@gmail.com>
2021-02-17 13:21:29 -0200

    feature: Fazendo o corpo da cobra atravessar a tela e receber comando de direção.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let canvas = document.getElementById('background-game');
 let context = canvas.getContext('2d');
+let direction = 'right';
 let box = 32;
 let snake = [];
 
@@ -7,6 +8,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+
 
 function createBackground(){
     context.fillStyle = 'lightgreen';
@@ -20,6 +22,55 @@ function createSnake(){
     }
 }
 
+function startGame(){
+    if(snake[0].x > (15 * box) && direction === "right"){
+        snake[0].x = 0;
+    }else if(snake[0].x < 0 && direction === "left"){
+        snake[0].x = (16 * box);
+    }else if(snake[0].y > (15 * box) && direction === "right"){
+        snake[0].y = 0;
+    }else if(snake[0].x < 0 && direction === "right"){
+        snake[0].y = (16 * box);
+    }
+    
+    createBackground();
+    createSnake();
 
-createBackground();
-createSnake();
+    let positionSnakeX = snake[0].x;
+    let positionSnakeY = snake[0].y;
+
+    if(direction === "right"){
+        positionSnakeX += box;
+    } else if(direction === "left"){
+        positionSnakeX -= box;
+    } else if(direction === "up"){
+        positionSnakeY -= box;
+    } else if(direction === "down"){
+        positionSnakeY += box;
+    }
+
+    snake.pop();
+
+    const newHead = {
+        x:  positionSnakeX,
+        y: positionSnakeY
+    };
+
+    snake.unshift(newHead);
+}
+
+function updateDirection(event){
+    if(event.keyCode === 37 && direction !== "right"){
+        direction = "left";
+    }else if(event.keyCode === 38 && direction !== "down"){
+        direction = "up";
+    }else if(event.keyCode === 39 && direction != "left"){
+        direction = "right";
+    }else if(event.keyCode === 40 && direction !== "up"){
+        direction = "down";
+    }
+}
+
+document.addEventListener('keydown', updateDirection);
+
+let game = setInterval(startGame, 100);
